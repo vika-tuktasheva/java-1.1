f539d607aec836793e1eddf7e7ccd7e6a7d3b2a0
vika-tuktasheva <konovalova_v@list.ru>
2022-03-21 22:24:08 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 100;
+        int amountOfRubles = 20;
+
+        int bonusMiles = price/amountOfRubles;
+
+        System.out.println("Вам начислены бонусные мили:"+bonusMiles);
+    }
+}
